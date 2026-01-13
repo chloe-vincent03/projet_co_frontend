e90cd50382dc67f5d0c2e9f38a325df236c8d086
Chloé Vincent <chloe.vincent03@edu.univ-fcomte.fr>
2026-01-13 15:57:33 +0100

    changement url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://vs2g6quq7gt.preview.hosting-ik.com",
   withCredentials: true,
 });
 
